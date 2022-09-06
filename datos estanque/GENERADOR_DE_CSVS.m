9c00e3277d316cdb3d8af5355dd9cf7f846a03ed
Joak01 <joaquin.ortega@ug.uchile.cl>
2022-09-06 02:10:57 -0300

    mas ejemplos añadidos

--- datos estanque/GENERADOR_DE_CSVS.m
+++ datos estanque/GENERADOR_DE_CSVS.m
@@ -9,4 +9,4 @@ data = [TIME, Referencia, HC, F_PID];
 
 DATOS_MODELO = [headers; num2cell(data)];
 
-writecell(DATOS_MODELO, 'datos con falla/data_falla_incipiente_bomba_v3.csv') %
+writecell(DATOS_MODELO, 'datos con falla/data_falla_incipiente_fuga_v4.csv') %
